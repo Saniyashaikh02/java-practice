44d57cf2dab00660c43c2082dea6695d86d32449
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-13 11:30:12 +0530

    Added methods examples

---
+++ core-java/methods/methods.java
@@ -0,0 +1,9 @@
+public class methods {
+    static void myMethod() {
+         System.out.println("I got placed in a good company");
+    }
+
+public static void main(String[] ars){
+            myMethod();
+        }
+    }
