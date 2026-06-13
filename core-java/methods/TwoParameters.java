a4a3dd57d652831e184a97adeccc9ff78476b9ac
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-13 12:59:36 +0530

    Added two parameters method example

---
+++ core-java/methods/TwoParameters.java
@@ -0,0 +1,9 @@
+public class TwoParameters {
+    static int myMethod( int x, int y) {
+        return x = y;
+    
+    }
+    public static void main (String[] args) {
+        System.out.println(myMethod(10, 20));
+    }
+}
