b35e49641e3fcc4f1f9431f6d66ac507bc19ecfc
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-13 12:06:03 +0530

    Added method parameters example

---
+++ core-java/methods/ParametersDemo.java
@@ -0,0 +1,11 @@
+public class ParametersDemo {
+    static void myMethod(String fname) {
+         System.out.println(fname + " shaikh");
+     }
+     public static void main(String[] args){
+        myMethod("saniya");
+        myMethod("amirah");
+        myMethod("sana");
+     }
+    }
+    
