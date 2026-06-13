16dc06110731afd11cedf8a95491e009dfc4ac96
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-13 13:17:52 +0530

    Added welcome message parameter example

---
+++ core-java/methods/WelcomeMessageDemo.java
@@ -0,0 +1,12 @@
+public class WelcomeMessageDemo {
+
+    static void welcomeMessage(String name) {
+        System.out.println("Welcome " + name);
+    }
+
+    public static void main(String[] args) {
+
+        welcomeMessage("Niharika");
+
+    }
+}
