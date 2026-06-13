19d1bff7c86b359586a758c3cce8f1862250b170
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-13 14:42:01 +0530

    Added recursion examples for countdown and call stack

---
+++ core-java/methods/CountdownRecursion.java
@@ -0,0 +1,17 @@
+public class CountdownRecursion {
+
+    static void countDown(int n) {
+
+        if (n == 0) {
+            System.out.println("Done");
+            return;
+        }
+
+        System.out.println(n);
+        countDown(n - 1);
+    }
+
+    public static void main(String[] args) {
+        countDown(3);
+    }
+}
