f0ab373cba32ee5f9cb2f24a137ac375c1f48e89
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-15 19:04:48 +0530

    added Default Constructor

---
+++ core-java/03-oop/DefaultConstructor.java
@@ -0,0 +1,7 @@
+public class DefaultConstructor {
+    int x;
+    public static void main(String[] args){
+        DefaultConstructor myObj = new DefaultConstructor();
+        System.out.println(myObj.x);    
+    }
+}
