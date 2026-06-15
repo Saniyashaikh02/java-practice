2764fdeee29ce22b8d7d8566231d304efc1830ea
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-15 12:38:56 +0530

    Add Java classes and objects practice

---
+++ core-java/03-oop/ClassesAndObject.java
@@ -0,0 +1,11 @@
+public class ClassesAndObject {
+     int x = 10;
+
+public static void main(String[] args){
+    ClassesAndObject m1= new ClassesAndObject();
+    ClassesAndObject m2= new ClassesAndObject();
+    System.out.println(m1.x);
+    System.out.println(m2.x);
+
+}
+}
