a4d6a293adfe7c5d77fc81efd2a9720d40fd7c50
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-15 12:52:00 +0530

    added Attributes

---
+++ core-java/03-oop/Attributes.java
@@ -0,0 +1,10 @@
+public class Attributes {
+    String fname = "Amirah";
+    String lname = "Shaik";
+    int age = 22;
+    public static void main(String[] args) {
+        Attributes myObj = new Attributes();
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+        System.out.println("Age: " + myObj.age);
+    }
+}
