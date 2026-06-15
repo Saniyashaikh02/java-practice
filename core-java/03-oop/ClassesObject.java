4d38e63e0604b09d061eda801a9b288fb23d830c
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-15 18:15:55 +0530

    added classes and object

---
+++ core-java/03-oop/ClassesObject.java
@@ -0,0 +1,11 @@
+public class ClassesObject {
+    
+String city = "London";
+int population = 9000000;
+public static void main(String[] args){
+ ClassesObject myObj = new ClassesObject();
+ClassesObject myObj2 = new ClassesObject();
+System.out.println(myObj.city);
+System.out.println(myObj.population);
+}
+}
