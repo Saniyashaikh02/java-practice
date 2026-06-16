abeb34eb2701ce8d2dd1d77bad07b45dbb1bea46
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-16 13:45:18 +0530

    added private access modifire

---
+++ core-java/04-access-modifiers/PrivateExample.java
@@ -0,0 +1,11 @@
+public class PrivateExample {
+
+    private int age = 21;
+
+    public static void main(String[] args) {
+
+        PrivateExample obj = new PrivateExample();
+
+        System.out.println(obj.age);
+    }
+}
