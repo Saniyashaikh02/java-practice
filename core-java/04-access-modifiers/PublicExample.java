436997283c2cec04b5dc141bc330260bf4677637
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-16 13:29:55 +0530

    added public modifires example

---
+++ core-java/04-access-modifiers/PublicExample.java
@@ -0,0 +1,15 @@
+ public class PublicExample{
+       public String name = "Amirah";
+       public void display() {
+        System.out.println("Name:" + name);
+       }
+       
+        public static void main(String[] args){
+            PublicExample obj = new PublicExample();
+            obj.display();
+    
+        }
+       }
+    
+    
+
