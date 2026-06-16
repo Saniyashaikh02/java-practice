a902d85011522415b46d52e7cb2f6f897b552140
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-16 14:22:16 +0530

    added Default example

---
+++ core-java/04-access-modifiers/DefaultExample.java
@@ -0,0 +1,8 @@
+ class DefaultExample {
+    String name ="Amirah";
+
+public static void main(String[] args) {
+    DefaultExample obj = new DefaultExample();
+    System.out.println(obj.name);
+}
+ }
