1b35b288371baa41bae8816a4063b07455a3b79c
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-18 12:20:28 +0530

    added BuiltIn Package Example

---
+++ core-java/07-packages/BuiltInPackageExample.java
@@ -0,0 +1,9 @@
+public class BuiltInPackageExample {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.max(10, 20));
+        System.out.println(Math.min(10, 20));
+    }
+}
