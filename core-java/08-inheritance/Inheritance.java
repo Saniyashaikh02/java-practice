b1223eecc662b99442ac5029caaa11cec517ed81
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-18 16:30:18 +0530

    added Inheritance example.java

---
+++ core-java/08-inheritance/Inheritance.java
@@ -0,0 +1,22 @@
+public class Inheritance {
+
+    protected String brand = "Ford";
+
+    public void honk() {
+        System.out.println("Tut, Tut!");
+    }
+}
+
+class Car extends Inheritance {
+
+    private String modelName = "Mustang";
+
+    public static void main(String[] args) {
+
+        Car myCar = new Car();
+
+        myCar.honk();
+
+        System.out.println(myCar.brand + " " + myCar.modelName);
+    }
+}
