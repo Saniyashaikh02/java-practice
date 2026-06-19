394813c227b71dca285bc0106b1a989eca63ee08
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-19 22:06:33 +0530

    added enum example

---
+++ core-java/15-enum/EnumExample.java
@@ -0,0 +1,16 @@
+enum Level {
+
+    LOW,
+    MEDIUM,
+    HIGH
+}
+
+public class EnumExample {
+
+    public static void main(String[] args) {
+
+        Level myLevel = Level.MEDIUM;
+
+        System.out.println(myLevel);
+    }
+}
