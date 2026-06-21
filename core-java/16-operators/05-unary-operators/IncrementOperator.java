a59fc770207968e88ddd5cdc5df3a8a2827b1cea
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-21 17:32:10 +0530

    added increment operator example

---
+++ core-java/16-operators/05-unary-operators/IncrementOperator.java
@@ -0,0 +1,8 @@
+public class IncrementOperator {
+    public static void main(String[] args){
+        int a = 20;
+        int b = ++a + a++;
+        System.out.print("value of a is " + a);
+        System.out.print("value of b is " + b);
+    }
+}
