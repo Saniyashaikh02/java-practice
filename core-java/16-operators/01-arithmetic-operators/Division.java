9bacac9c4e6db22e6fad7bb302592639f0a83cf4
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-21 16:54:57 +0530

    added division example

---
+++ core-java/16-operators/01-arithmetic-operators/Division.java
@@ -0,0 +1,8 @@
+public class Division {
+    public static void main(String[] args){
+        int a = 20;
+        int b = 10;
+        int c = a / b;
+        System.out.println("Division of a and b is: " + c);
+    }
+}
