5cdb361f760256e810937a9491eda520f613d99e
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-21 16:52:27 +0530

    added substraction example

---
+++ core-java/16-operators/01-arithmetic-operators/Subtraction.java
@@ -0,0 +1,8 @@
+public class Subtraction {
+    public static void main(String[] args){
+        int a = 20;
+        int b = 10;
+        int c = a - b;
+        System.out.println("Difference of a and b is: " + c);
+    }
+}
