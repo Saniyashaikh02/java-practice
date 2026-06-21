768ae672720c5b94cb8c5ecdbf61e138de3ff945
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-21 17:41:46 +0530

    added ternanry operator example

---
+++ core-java/16-operators/06-ternary-operator/TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args){
+        int a = 20;
+        int b = 10;
+        String result = (a > b) ? "a is greater than b" : "a is less than b";
+        System.out.println(result);
+    }
+}
