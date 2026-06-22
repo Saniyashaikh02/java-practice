a3e98b145c5cddd154e7a403bf20cafc2faa9fb2
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-22 14:05:20 +0530

    added

---
+++ core-java/18-program-example/Demo5.java
@@ -0,0 +1,8 @@
+public class Demo5 {
+    public static void main(String[] args){
+        for(int x=1; x < 10;){
+            System.out.println("hello world");
+            x++;
+        }
+    }
+}
