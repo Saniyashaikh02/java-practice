9b8e89e66ed35dd3945178317c495616ce9bda39
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-22 11:35:00 +0530

    added if else statement using && logical operator

---
+++ core-java/17-control-flow/02-if-else/Demo2.java
@@ -0,0 +1,11 @@
+ class Demo2 {
+    public static void main(String[] args){
+        String visa = "yes";
+        String passport ="yes";
+        if(visa.equals("yes") && passport.equals("yes")){
+            System.out.println("You are eligible to travel.");
+        } else {
+            System.out.println("You are not eligible to travel.");
+        }
+    }
+}
