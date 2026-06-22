74e1daec395e887ac00798bd3a94742fd6248bc4
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-22 12:10:13 +0530

    for each loop example

---
+++ core-java/17-control-flow/05-for-loop/ForEachLoop.java
@@ -0,0 +1,16 @@
+public class ForEachLoop {
+
+    public static void main(String[] args) {
+
+        String[] fruits = {
+                "Apple",
+                "Banana",
+                "Mango"
+        };
+
+        for (String fruit : fruits) {
+
+            System.out.println(fruit);
+        }
+    }
+}
