d02de3bc3731efbcdd7787b49bde00dc612461a2
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-22 13:04:48 +0530

    added check number is positive or negative

---
+++ core-java/18-program-example/Demo1.java
@@ -0,0 +1,14 @@
+public class Demo1 {
+    public static void main(String[] args){
+        int a = 1;
+        if(a ==0){
+            System.out.println("number is zero");
+        }else if(a>=0){
+            System.out.println("number is positive");
+
+        }else{
+            System.out.println("number is negative");
+        }
+        }
+    }
+
