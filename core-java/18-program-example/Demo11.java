308f744aa79de0ba754a5a778d02aadc7812b989
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-22 14:45:38 +0530

    added show stars

---
+++ core-java/18-program-example/Demo11.java
@@ -0,0 +1,13 @@
+public class Demo11 {
+    public static void main(String[] args){
+        for(int i = 0; i<5; i++){
+            if(i<5){
+                System.out.print("*");
+
+            }
+               System.out.println("*");
+    
+    }
+
+    }
+}
