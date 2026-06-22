824b91856ab5f315397689315978370643ff8057
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-22 14:21:51 +0530

    added program to print number from 1 o 20 which are divisible by 2 & 7

---
+++ core-java/18-program-example/Demo8.java
@@ -0,0 +1,10 @@
+public class Demo8 {
+   public static void main(String[] args){
+    int i;
+    for(i =1; i<=20; i++){
+        if(i %2 ==0 && i %7==0){
+            System.out.println(i);
+        }
+    }
+   } 
+}
