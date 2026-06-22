31e1729d8aa3674d821b0802d58a488c5fd50e46
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-22 11:11:02 +0530

    added if statment example

---
+++ core-java/17-control-flow/01-if-statement/IfStatement.java
@@ -0,0 +1,10 @@
+public class IfStatement {
+    public static void main (String[] args ){
+        int age = 18;
+        if (age >= 18){
+        System.out.println("You are eligible to vote.");
+        }
+       
+    }
+    }
+
