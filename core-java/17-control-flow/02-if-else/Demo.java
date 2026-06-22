86d54dbd34e0dbbc31f91c7eef621471cd558c85
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-22 11:31:12 +0530

    added if else statement using relational operator

---
+++ core-java/17-control-flow/02-if-else/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String[] args){
+      int total = 10000;
+        int gst = 18;
+        if(total >= 10000 || gst >= 18){
+            System.out.println("You are eligible to pay GST.");
+        } else {
+            System.out.println("You are not eligible to pay GST.");
+        }
+    }
+    
+}
