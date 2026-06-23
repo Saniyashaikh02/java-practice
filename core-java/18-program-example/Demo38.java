2ab529ce2b78ead620a4704a03a79875330a86e7
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 14:25:32 +0530

    Added method returning sum of parameter

---
+++ core-java/18-program-example/Demo38.java
@@ -0,0 +1,10 @@
+public class Demo38 {
+    public static void main(String[] args){
+        Demo38 obj = new Demo38();
+        int a = obj.addition(10);
+        System.out.println("in main method" + a);
+    }
+    int addition(int a){
+        return a +a;
+    }
+}
