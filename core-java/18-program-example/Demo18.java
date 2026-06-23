67da1f7f3777402d57a06f690879129ac670492a
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 11:27:36 +0530

    added square patter example

---
+++ core-java/18-program-example/Demo18.java
@@ -0,0 +1,10 @@
+public class Demo18 {
+    public static void main(String[] args){
+        for(int i =1; i<= 4; i++){
+            System.out.println("*" + "*" + "*"+ "*");
+            if(i %4==0){
+                System.out.println();
+            }
+        }
+    }
+}
