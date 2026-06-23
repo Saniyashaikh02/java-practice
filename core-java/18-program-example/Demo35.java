454830821d78ec7ef9290dbb3e1966eaf9ca6f4c
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 14:09:13 +0530

    Added method with string return type example

---
+++ core-java/18-program-example/Demo35.java
@@ -0,0 +1,12 @@
+public class Demo35 {
+    public static void  main(String[] args){
+        Demo35 obj = new Demo35();
+        String a = obj.varad();
+        System.out.println("in main method");
+
+    }
+    String varad(){
+        System.out.println("in fun method");
+        return "ingle";
+    }
+}
