c0ffeb2b3d76dd6d649dcfd323f8f557636c0a9b
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 11:38:27 +0530

    added right-angled triangle star pattern example

---
+++ core-java/18-program-example/Demo19.java
@@ -0,0 +1,21 @@
+public class Demo19 {
+    public static void main(String[] args){
+        for( int i =1; i<=4; i++){
+            if( i==1){
+                System.out.println("*");
+            }
+            
+            if(i==2){
+                System.out.println("*" + "*");
+            }
+           
+            if(i==3){
+                System.out.println("*" + "*" + "*");
+            }
+            
+            if(i==4){
+                System.out.println("*" + "*" + "*" + "*");
+            }
+        }
+    }
+}
