cfcf2d56ce4eb7ec59cda99b5563300415fcb31c
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 21:07:43 +0530

    Add Demo1 Java program with marks array

---
+++ core-java/20-arrays/Demo1.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+public class Demo1 {
+    public static void main(String[] args){
+        int marks[] = {10,20,30,40,50};
+        System.out.println(Arrays.toString(marks));
+    }
+}
