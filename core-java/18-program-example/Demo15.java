81b090c6230edde91a72789954298aac419416b2
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 11:06:13 +0530

    added to Show 1A 1B 1C value

---
+++ core-java/18-program-example/Demo15.java
@@ -0,0 +1,9 @@
+public class Demo15 {
+    public static void main(String[] args){
+        char c ='A';
+        for(int i =1; i<=3; i++){
+            System.out.println(i + " " + (c++) + "");
+
+        }
+    }
+}
