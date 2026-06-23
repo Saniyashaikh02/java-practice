24b822db6ff80533f594d605f2779f234f7707b5
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 13:45:58 +0530

    Added switch statement with character variable

---
+++ core-java/18-program-example/Demo32.java
@@ -0,0 +1,20 @@
+public class Demo32 {
+    public static void main(String[] args){
+        char ch ='a';
+        switch(ch){
+            default:
+                System.out.println("more money required");
+                break;
+
+                case 100:
+                    System.out.println("kunafa");
+                    break;
+
+                    case 200:
+                        System.out.println("waffle");
+                        break;
+        
+    }
+}
+}
+
