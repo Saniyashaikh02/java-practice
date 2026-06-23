089e5abc8ddb5874b2c58ed705a25ae52b24e063
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 13:59:10 +0530

    Added object method calling example

---
+++ core-java/18-program-example/Demo34.java
@@ -0,0 +1,12 @@
+public class Demo34 {
+    public static void main(String[] args){
+        Demo34 v = new Demo34();
+        v.fun();
+        System.out.println("in main method");
+    
+
+    }
+    void fun(){
+        System.out.println("in fun method");
+    }
+}
