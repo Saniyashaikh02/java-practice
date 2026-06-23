4a5a5ffd5f1a74c13c2f1052f394a731b087f658
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 12:31:32 +0530

    Added consecutive number pattern using nested loops

---
+++ core-java/18-program-example/Demo25.java
@@ -0,0 +1,11 @@
+public class Demo25 {
+    public static void main(String[] args) {
+        int n =1;
+        for(int i=1; i<=3; i++){
+            for(int j=1; j<=4; j++){
+                System.out.print((n++)+ "");
+            }
+            System.out.println();
+        }
+    }
+}
