304f057bec74a0ae51825c81c1aa709b534c161a
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 22:20:53 +0530

    Add array input example using Scanner

---
+++ core-java/20-arrays/multi-dimensional-array/Demo9.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Demo9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 array elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements are:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        sc.close();
+    }
+}
