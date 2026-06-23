c32abd289787d29ba67dc57827b2f46ff51d2557
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 11:00:39 +0530

    added to Show ASCII values ACEG

---
+++ core-java/18-program-example/Demo14.java
@@ -0,0 +1,9 @@
+public class Demo14 {
+    public static void main(String[] args) {
+        for (char i = 65; i <= 71; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
