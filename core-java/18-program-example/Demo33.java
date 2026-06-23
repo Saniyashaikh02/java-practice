bf90dd268a4bae2657f90bbdfb58f55a08e0cee0
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 13:53:27 +0530

    Added switch statement using string values

---
+++ core-java/18-program-example/Demo33.java
@@ -0,0 +1,13 @@
+public class Demo33 {
+    public static void main(String[] args){
+        String v = "varad";
+        switch (v) {
+            default:
+                System.out.println("smart");
+                break;
+                case "varad":
+                    System.out.println("handsome");
+                    break;
+        }
+    }
+}
