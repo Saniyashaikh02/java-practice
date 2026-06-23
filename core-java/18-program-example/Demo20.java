9be77b0ab9879c686318e83ce1894994c83b0860
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 11:54:49 +0530

    Print ABCD and DCBA pattern

---
+++ core-java/18-program-example/Demo20.java
@@ -0,0 +1,14 @@
+public class Demo20 {
+    public static void main(String[] args) {
+
+        for (char ch = 'A'; ch <= 'D'; ch++) {
+            System.out.print(ch);
+        }
+
+        System.out.println();
+
+        for (char ch = 'D'; ch >= 'A'; ch--) {
+            System.out.print(ch);
+        }
+    }
+}
