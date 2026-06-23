c49cbfbb36a8254fe14419ba91ab3fd12255f569
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 11:11:07 +0530

    added to Show Aa Bb Cc values

---
+++ core-java/18-program-example/Demo16.java
@@ -0,0 +1,9 @@
+public class Demo16  {
+    public static void main(String[] args){
+        char ch = 'a' ; char chi = 'A';
+        for(int i =1; i<=3; i++){
+            System.out.println((chi++) + " " + (ch++) + " ");
+        }
+    }
+    
+}
