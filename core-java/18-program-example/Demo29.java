9f7baf9ab7b4f7183a92626e67c66d314a3c5b75
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-23 13:27:17 +0530

    Added basic switch statement example

---
+++ core-java/18-program-example/Demo29.java
@@ -0,0 +1,14 @@
+public class Demo29{
+    public static void main(String[] args){
+        int money = 1;
+        switch(money){
+            case 1:
+                System.out.println("panner");
+              
+            case 2:
+                System.out.println("kaju");
+                break;
+                
+        }
+    }
+}
