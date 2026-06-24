a4b58f767f4f676743134948cb1049813347ca78
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-24 18:04:42 +0530

    Print all characters of a string using charAt() and loop

---
+++ core-java/22-strings/Demo17.java
@@ -0,0 +1,10 @@
+public class Demo17 {
+    public static void main(String[] args) {
+
+        String s = "codegen technologies";
+
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+        }
+    }
+}
