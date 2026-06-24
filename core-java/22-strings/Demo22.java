8dcb8e75118a885bfcbe954173fa8d1efb0dd762
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-24 18:31:35 +0530

    Demonstrate StringBuffer capacity after append operation

---
+++ core-java/22-strings/Demo22.java
@@ -0,0 +1,7 @@
+public class Demo22 {
+   public static void main(String[] args){
+    StringBuffer sb = new StringBuffer();
+    sb.append("hello");
+    System.out.println(sb.capacity());
+   } 
+}
