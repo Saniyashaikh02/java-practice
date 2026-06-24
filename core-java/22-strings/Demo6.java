94f703f80d19a20626e511f1722e025069b78d0e
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-24 14:06:24 +0530

    Implemented string comparison logic

---
+++ core-java/22-strings/Demo6.java
@@ -0,0 +1,11 @@
+public class Demo6 {
+   public static void main(String[] args){
+    String s =new String ("Amirah");
+    String s1 =new String ("Amirah");
+    if(s==s1){
+        System.out.println("same");
+    }else{
+        System.out.println("not same");
+    }
+   } 
+}
