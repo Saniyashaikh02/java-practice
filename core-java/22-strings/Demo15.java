1536271c32fde872a2ae7702a03d5d2c138ed87a
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-24 17:46:04 +0530

    Demonstrate String case conversion using toLowerCase() and toUpperCase()

---
+++ core-java/22-strings/Demo15.java
@@ -0,0 +1,8 @@
+public class Demo15 {
+    public static void main(String[] args){
+        String s = "Amirah";
+        String s1 ="varad";
+        System.out.println(s.toLowerCase());
+        System.out.println(s1.toUpperCase());
+    }
+}
