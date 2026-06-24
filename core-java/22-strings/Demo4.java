6d953db23ba4d5ab33933dd8dcf7adfd7212f8a4
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-24 12:02:28 +0530

    Add string comparison example using equality operator

---
+++ core-java/22-strings/Demo4.java
@@ -0,0 +1,11 @@
+public class Demo4 {
+   public static void main(String[] args){
+    String a = "amirah";
+    String a1 = "amirah";
+    if(a ==a1){
+        System.out.println(true);
+    }else{
+        System.out.println(false);
+    }
+   } 
+}
