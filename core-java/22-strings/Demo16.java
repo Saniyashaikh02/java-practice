60eb2f7e8cdc022339f2fa86cb6d74cd5921d029
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-24 17:56:18 +0530

    Demonstrate String trim() method to remove leading and trailing spaces

---
+++ core-java/22-strings/Demo16.java
@@ -0,0 +1,7 @@
+public class Demo16 {
+    public static void main(String[] args){
+        String s = "Codgen technologies";
+        String s1 = s.trim();
+        System.out.println(s1);
+    }
+}
