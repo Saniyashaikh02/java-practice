debc4b9531c6848e646649a0a2ac8f95243b2b92
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-24 11:51:55 +0530

    Add string concatenation example using concat method

---
+++ core-java/22-strings/Demo1.java
@@ -0,0 +1,9 @@
+public class Demo1 {
+    public static void main(String[] args){
+        String a = "amirah";
+        String a1 = a.concat("varad");
+        System.out.println(a1);
+
+
+    }
+}
