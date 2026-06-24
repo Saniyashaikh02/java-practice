63c4b5c888523ebc9a913d353b3ace9489e0a778
Saniyashaikh02 <shaikhsaniya1021@gmail.com>
2026-06-24 12:05:32 +0530

    Add string comparison using == and equals method

---
+++ core-java/22-strings/Demo5.java
@@ -0,0 +1,12 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        String var1 = "amirah";
+        String var2 = new String("amirah");
+
+        if (var1 == var2) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
